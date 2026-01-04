dfd8df7100e2bc290846d60397ef2b1237d7109e
Dale Morgan <mail@dalemorgan.us>
2026-01-04 10:49:36 +0000

    formatting enhanced

--- modules/default.nix
+++ modules/default.nix
@@ -1,6 +1,8 @@
 { lib, ... }:
 {
-  imports = map (f: ./. + "/${f}")
-    (builtins.filter (lib.hasSuffix ".nix")
-      (lib.subtractLists ["default.nix" "home.nix"] (builtins.attrNames (builtins.readDir ./.))));
+  imports = map (f: ./. + "/${f}") (
+    builtins.filter (lib.hasSuffix ".nix") (
+      lib.subtractLists [ "default.nix" "home.nix" ] (builtins.attrNames (builtins.readDir ./.))
+    )
+  );
 }
